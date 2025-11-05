8ed0c1e1aecb230366c817f5aa3bea4db0578e78
Saad05-dev <bougrasaad@gmail.com>
2025-11-05 12:26:27 +0100

    Changing result calculation to manual instead of using function eval

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 // Variable that stores the buttons values
-let calc = '' || localStorage.getItem('result') ;;
-
-calculation();
+let calc = localStorage.getItem('result') || '' ;
+displayCurrentValue();
 //Function that captures user input and stores it in the variable calc 
-function updateCalculation(operation)
+function handleCalculation(operation)
 {
-    calc += operation;
+    if( operation == "+" || operation == "-" || operation == "*" || operation == "/")
+    {
+        calc += ` ${operation} `;
+    }
+    else
+    {
+        calc += operation;
+    }
 
-    calculation();
+    displayCurrentValue();
+
 
     localStorage.setItem('result',calc);
 }
 // Function that displays the input of user and result
-function calculation()
+function displayCurrentValue()
 {
-    document.querySelector('.display').innerHTML = calc;
+    document.querySelector('.display').innerHTML = " " + calc;
+}
+function calculateResult()
+{
+    let operation = calc.trim().split(' ');
+    console.log(operation);
+    
+    switch (operation[1]) {
+       case "+":
+                return operation[0] + operation[2];
+
+            case "-":
+                return operation[0] - operation[2];
+
+            case "*":
+                return operation[0] * operation[2];
+
+            case "/":
+                return operation[0] / operation[2];
+
+            default:
+                return alert("unknown operation try again");
+    }
 }
